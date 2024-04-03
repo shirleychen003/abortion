7407d4f2619a56717ee0533995fa2445368dc42e
shirkey <sshirleyy.chen@mail.utoronto.ca>
2024-04-02 22:41:02 -0400

    rest of the commits

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Cleans the data
+# Purpose: Filters the necessary variables from the large converted dataset
 # Author: Shirley Chen
 # Date: 1 April 2024
 # Contact: sshirleyy.chen@mail.utoronto.ca
@@ -13,58 +13,27 @@ library(tidyverse)
 library(knitr)
 library(janitor)
 
-# # Open data
-# raw_GSS_data<-
-#   read_csv(
-#     "inputs/data/raw_GSS_data.csv",
-#     show_col_types = FALSE
-#   )
-
+# Open data
 raw_GSS_data <-
   read_csv(
-    "outputs/data/cleaned_GSS_data.dsv",
+    "inputs/data/raw_GSS_data.csv",
     show_col_types = FALSE
-       )
-  
-
-# Cleaning
-cleaned_GSS_data <-
-  clean_names(raw_GSS_data) |>
-  drop_na(year, age, abany, abpoor, abhlth, abrape)
-
-# Remove uncessary columns
-cleaned_GSS_data <-
-  subset(cleaned_GSS_data, select = c(year, age, sex, 
-                                      abany, abpoor, abhlth, abrape))
-
-# Keep years 2016, 2018, 2021, 2022
-selected_years <- c("2016", "2018", "2021", "2022")
-cleaned_GSS_data <- cleaned_GSS_data |>
-  filter(year %in% selected_years)
-
-# Rename variable responses
-cleaned_GSS_data_renamed <- mutate(cleaned_GSS_data,
-  abany = recode(abany, '1' = 'yes', '2' = 'no'),
-  abpoor = recode(abpoor, '1' = 'yes', '2' = 'no'),
-  abhlth = recode(abhlth, '1' = 'yes', '2' = 'no'),
-  abrape = recode(abrape, '1' = 'yes', '2' = 'no')
-)
-
-# Rename columns to better understand
-cleaned_GSS_data <- cleaned_GSS_data |>
-  rename(
-    any_reason = abany,
-    low_income = abpoor,
-    endangered_health = abhlth,
-    rape = abrape
   )
 
-# Rename sex responses
-cleaned_GSS_data <-cleaned_GSS_data |>
-  mutate(sex = recode(sex,
-                 '1' = 'male',
-                 '2' = 'female'))
+# Initial Cleaning
+ filtered_GSS_data <-
+  clean_names(raw_GSS_data) |>
+  drop_na(year, age, relig, abany, abpoor, abhlth, abrape)
 
+# Remove unnecessary columns
+filtered_GSS_data <-
+  subset(filtered_GSS_data, select = c(year, age, sex, relig,
+                                      abany, abpoor, abhlth, abrape))
+
+# Keep years 2010, 2014, 2018,
+selected_years <- c("2010", "2014", "2018")
+filtered_GSS_data <- filtered_GSS_data |>
+  filter(year %in% selected_years)
 
 #### Save data ####
-write_csv(cleaned_GSS_data, "outputs/data/cleaned_GSS_data.csv")
+write_csv(filtered_GSS_data, "outputs/data/filtered_GSS_data.csv")
---
+++ scripts/02a-data_cleaning.R
@@ -0,0 +1,68 @@
+#### Preamble ####
+# Purpose: Cleans the filtered data
+# Author: Shirley Chen
+# Date: 1 April 2024
+# Contact: sshirleyy.chen@mail.utoronto.ca
+# License: MIT
+# Pre-requisites:
+# 00-simulate_data.R
+# 01-download_data.R
+
+#### Workspace setup ####
+library(tidyverse)
+library(knitr)
+library(janitor)
+
+# Open data
+cleaned_GSS_data <-
+  read_csv(
+    "outputs/data/filtered_GSS_data.csv",
+    show_col_types = FALSE
+  )
+
+
+# Rename columns to better understand
+cleaned_GSS_data <- cleaned_GSS_data |>
+  rename(
+    religion = relig,
+    any_reason = abany,
+    low_income = abpoor,
+    endangered_health = abhlth,
+    rape = abrape
+  )
+
+# Rename religion responses
+cleaned_GSS_data <-cleaned_GSS_data |>
+  mutate(religion = recode(religion,
+                      '1' = 'Protestant',
+                      '2' = 'Catholic',
+                      '3' = 'Jewish',
+                      '4' = 'None',
+                      '5' = 'Other',
+                      '6' = 'Buddhism',
+                      '7' = 'Hinduism',
+                      '8' = 'Other eastern religions', 
+                      '9' = 'Muslim/Islam',
+                      '10' = 'Orthodox-Christian',
+                      '11' = 'Christian',
+                      '12' = 'Native American',
+                      '13' = 'Inter-nondenominational'))
+
+# Rename sex responses
+cleaned_GSS_data <-cleaned_GSS_data |>
+  mutate(sex = recode(sex,
+                      '1' = 'male',
+                      '2' = 'female'))
+
+# Rename variable responses
+cleaned_GSS_data <-cleaned_GSS_data |>
+  mutate(any_reason = recode(any_reason, 
+                             '1' = 'yes', '2' = 'no'))
+cleaned_GSS_data <-cleaned_GSS_data |>
+  mutate(low_income = recode(low_income, 
+                             '1' = 'yes', '2' = 'no'))
+cleaned_GSS_data <-cleaned_GSS_data |>
+  mutate(endangered_health = recode(endangered_health, 
+                             '1' = 'yes', '2' = 'no'))
+cleaned_GSS_data <-cleaned_GSS_data |>
+  mutate(rape = rec
