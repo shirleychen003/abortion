3c43c80fff12c1d653985ef1d8db92a24b552080
shirkey <sshirleyy.chen@mail.utoronto.ca>
2024-04-03 20:05:51 -0400

    simulation script

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
-# Purpose: Simulates the change in rating of importance from 1998 to 2016
-# Author: Jessica Im
-# Date: 1 March 2024
-# Contact: jessica.im@mail.utoronto.ca
+# Purpose: Simulates the change in responses for abortion from 2006 to 2016
+# Author: Shirley Chen
+# Date: 1 April 2024
+# Contact: sshirleyy.chen@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: None
 # Any other information needed? N/A
@@ -11,22 +11,23 @@
 #### Workspace setup ####
 library(tidyverse)
 library(janitor)
+
 set.seed(250)
 
-# 160 selected to represent total participants and years selected based on GSS data
+# 200 selected to represent total participants and years selected based on GSS data
 #### Simulate data ####
 simulated_data <-
   tibble(
-    "Participant" = 1:160,
+    "Participant" = 1:200,
     Response = sample(
       x = c(1:5),
-      size = 160,
+      size = 200,
       replace = TRUE
     ),
-    # Randomly pick an option, with replacement, 160 times
+    # Randomly pick an option, with replacement, 200 times
     "Year" = sample(
-      x = c("1989", "1998", "2006", "2016"),
-      size = 160,
+      x = c("2006", "2016"),
+      size = 200,
       replace = TRUE
     )
   )
@@ -35,21 +36,17 @@ simulated_data <-
 #### Simulate cleaned data ####
 simulated_data_clean <-
   tibble(
-    "Year" = sample(
-      x = c(1989, 1998, 2006, 2016),
-      size = 4,
-      replace = FALSE
-      ),
-    Important = sample(
+    "Year" = c(2006, 2016),
+    "Yes" = sample(
       x = c(18:60),
-      size = 4,
+      size = 2,
       replace = TRUE
     ),
-    Very_Important = sample(
+    "No" = sample(
       x = c(25:80),
-      size = 4,
+      size = 2,
       replace = TRUE
-    ),
+    )
   )
 
 # Pivot and graph simulated data to create a connected dot graph
@@ -58,7 +55,7 @@ simulated_data_clean <-
 simulated_data_pivot <-
   simulated_data_clean |>
   pivot_longer(
-    cols = c("Important", "Very_Important"),
+    cols = c("Yes", "No"),
     names_to = "Response_Type",
     values_to = "Response_Proportion"
   )
@@ -67,9 +64,9 @@ simulated_data_pivot <-
 simulated_data_pivot |>
   ggplot(mapping = aes(x = Year, y = Response_Proportion, fill = Response_Type)) +
   geom_line(color="grey") +
-  geom_point(shape=21, color="black", fill="#69b3a2", size=2) +
+  geom_point(shape=21, color="black", fill="blue", size=3) +
   theme_minimal() +
-  labs(x = "Year", y = "Proportion Important (%)", caption = "Data source: World Bank.")
+  labs(x = "Year", y = "Proportion of Responses (%)", caption = "Data source: GSS.")
 
 
 
