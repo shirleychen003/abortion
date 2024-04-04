045f95a7fe485f35dd842ceda471f8fefdfca248
Shirley C <113147956+shirleychen003@users.noreply.github.com>
2024-04-03 23:40:54 -0400

    Update 02a-data_cleaning.R

--- scripts/02a-data_cleaning.R
+++ scripts/02a-data_cleaning.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Cleans the filtered data
 # Author: Shirley Chen
-# Date: 1 April 2024
+# Date: 1 April, 2024
 # Contact: sshirleyy.chen@mail.utoronto.ca
 # License: MIT
 # Pre-requisites:
