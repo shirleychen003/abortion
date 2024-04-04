084e37a3878bd0a536ccec431eb22b76b35cd24f
shirkey <sshirleyy.chen@mail.utoronto.ca>
2024-04-03 20:21:09 -0400

    test data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,66 +1,77 @@
 #### Preamble ####
-# Purpose: Testing 02-data_cleaning.R
-# Author: Jessica Im
-# Date: 11 March 2024
-# Contact: jessica.im@mail.utoronto.ca
+# Purpose: Testing 02a-data_cleaning.R
+# Author: Shirley chen
+# Date: 1 April 2024
+# Contact: sshirleyy.chen@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: Download 02-data_cleaning.R
+# Pre-requisites: Download 02a-data_cleaning.R
 # Any other information needed? N/A
 
 
 #### Workspace setup ####
 library(tidyverse)
 
-
 #### Test data ####
-clean_GSS_data<-
+cleaned_GSS_data<-
   read_csv(
     "outputs/data/cleaned_GSS_data.csv",
     show_col_types = FALSE
   )
 
 
-# Check for missing values in responses to question "helping_others"
-# Code references from: https://github.com/shirleychen003/gender-wage-gap/blob/main/scripts/03-test_data.R
-missing_values <- sum(is.na(clean_GSS_data$helping_others))
+# Check for missing values in responses to abortion questions 
+missing_values <- sum(is.na(cleaned_GSS_data$rape))
 cat("Missing values in question_response:", missing_values, "\n")
 
 
-# Check that responses range from 'very important' to 'not important at all'
-# Code referenced from: https://github.com/InessaDeAngelis/Perceptions_of_Women_in_Politics/blob/main/scripts/03-test_data.R
-class(clean_GSS_data$helping_others) == "character"
-sum(!(unique(clean_GSS_data$helping_others) %in%
-        c("very_important",
-        "important",
-        "neither",
-        "not_important",
-        "not_important_at_all",
+# Check that abortion question responses are "yes" or "no"
+class(cleaned_GSS_data$rape) == "character"
+sum(!(unique(cleaned_GSS_data$rape) %in%
+        c("yes",
+          "no",
           NA))) == 0
 
-class(clean_GSS_data$interesting_work) == "character"
-sum(!(unique(clean_GSS_data$interesting_work) %in%
-        c("very_important",
-          "important",
-          "neither",
-          "not_important",
-          "not_important_at_all",
+class(cleaned_GSS_data$endangered_health) == "character"
+sum(!(unique(cleaned_GSS_data$endangered_health) %in%
+        c("yes",
+          "no",
           NA))) == 0
 
-class(clean_GSS_data$social_usefulness) == "character"
-sum(!(unique(clean_GSS_data$social_usefulness) %in%
-        c("very_important",
-          "important",
-          "neither",
-          "not_important",
-          "not_important_at_all",
+class(cleaned_GSS_data$low_income) == "character"
+sum(!(unique(cleaned_GSS_data$low_income) %in%
+        c("yes",
+          "no",
+          NA))) == 0
+
+class(cleaned_GSS_data$any_reason) == "character"
+sum(!(unique(cleaned_GSS_data$any_reason) %in%
+        c("yes",
+          "no",
+          NA))) == 0
+
+# Check all the religion responses
+class(cleaned_GSS_data$religion) == "character"
+sum(!(unique(cleaned_GSS_data$religion) %in%
+        c("Protestant",
+        "Catholic",
+        "Jewish",
+        "None",
+        "Other",
+        "Buddhism",
+        "Hinduism",
+        "Other eastern religions",
+        "Muslim/Islam",
+        "Orthodox-Christian",
+        "Christian",
+        "Native American",
+        "Inter-nondenominational",
           NA))) == 0
 
 
-# Check that years are '1989', '1998', '2006', and '2016'
-class(clean_GSS_data$year) == "character"
-sum(!(unique(clean_GSS_data$year) %in%
-        c("1989",
-          "1998",
-          "2006",
+
+# Check that years are '2006' and '2016'
+class(cleaned_GSS_data$year) == "numeric"
+sum(!(unique(cleaned_GSS_data$year) %in%
+        c("2006",
           "2016",
           NA))) == 0
